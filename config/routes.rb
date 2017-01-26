c1950d4c223af1fe00920faf4f77614b6cf1847c
Francisco J Trujillo <francisco.jose1982@gmail.com>
2017-01-26 12:14:47 -0600

    modifying routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'portfolio/index'
+root 'portfolio#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
